55c75c596e13bf9bde111b832f8dd21342de3f23
KYs <amitkys59@gmail.com>
2026-04-22 10:16:24 +0530

    adding forced dynamic to candidate regs

--- app/add/candidate/page.tsx
+++ app/add/candidate/page.tsx
@@ -1,6 +1,8 @@
 import { createId } from "@paralleldrive/cuid2";
 import { redirect } from "next/navigation";
 
+export const dynamic = "force-dynamic";
+
 export default function Page() {
   const cuid = createId();
   redirect(`/add/candidate/${cuid}`);
