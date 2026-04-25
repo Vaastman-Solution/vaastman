4ff85d8cdc893608a53cd494c5a6abd59a9eb36d
KYs <amitkys59@gmail.com>
2026-04-25 14:40:35 +0530

    feat(admin-panel): add dashboard shell

---
+++ lib/menu-list.ts
@@ -0,0 +1,89 @@
+import {
+  Bookmark,
+  LayoutGrid,
+  type LucideIcon,
+  Settings,
+  SquarePen,
+  Tag,
+  Users,
+} from "lucide-react";
+
+type Submenu = {
+  href: string;
+  label: string;
+  active?: boolean;
+};
+
+type Menu = {
+  href: string;
+  label: string;
+  active?: boolean;
+  icon: LucideIcon;
+  submenus?: Submenu[];
+};
+
+type Group = {
+  groupLabel: string;
+  menus: Menu[];
+};
+
+export function getMenuList(pathname: string): Group[] {
+  return [
+    {
+      groupLabel: "",
+      menus: [
+        {
+          href: "/dashboard",
+          label: "Dashboard",
+          icon: LayoutGrid,
+          submenus: [],
+        },
+      ],
+    },
+    // {
+    //   groupLabel: "",
+    //   menus: [
+    //     {
+    //       href: "",
+    //       label: "Posts",
+    //       icon: SquarePen,
+    //       submenus: [
+    //         {
+    //           href: "/posts",
+    //           label: "All Posts",
+    //         },
+    //         {
+    //           href: "/posts/new",
+    //           label: "New Post",
+    //         },
+    //       ],
+    //     },
+    //     {
+    //       href: "/categories",
+    //       label: "Categories",
+    //       icon: Bookmark,
+    //     },
+    //     {
+    //       href: "/tags",
+    //       label: "Tags",
+    //       icon: Tag,
+    //     },
+    //   ],
+    // },
+    {
+      groupLabel: "",
+      menus: [
+        {
+          href: "/college",
+          label: "Add Colleges",
+          icon: Bookmark,
+        },
+        {
+          href: "/add/candidate",
+          label: "Internship",
+          icon: Users,
+        },
+      ],
+    },
+  ];
+}
